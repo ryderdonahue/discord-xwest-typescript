06342bc37fb2ecddc0b6f29906d7bd30f86ec7cf
Ryder Donahue <ryder.donahue@gmail.com>
2020-06-18 23:50:26 -0700

    first commit

---
+++ src/commands.ts
@@ -0,0 +1,8 @@
+export const ServerCommands = {
+    roll: "roll",
+    wipe: "wipe",
+    consolidate: "consolidate",
+    help: "help",
+    request: "request",
+    poll: "poll"
+}
---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const Constants = {
+    helpMessage: `XWest has several commands you can run
+    \n\`!roll d[number]\` will roll a dice - example: \`!roll d6\` will roll a six sided dice.
+    \n\`!poll [minutes] [options]\` will start a poll - example: \`!poll 5 Apex CoD PUBG\` will start a poll between these three games that will close in 5 minutes.
+    \n\`!request [feature]\` will log a feature request for XWestbot
+     `
+}
+
+export const EmojiArray = [
+    { discordValue: "%F0%9F%8D%87", emojiValue: '🍇' },
+    { discordValue: "%F0%9F%8D%8E", emojiValue: '🍎' },
+    { discordValue: "%F0%9F%8D%8C", emojiValue: '🍌' },
+    { discordValue: "%F0%9F%8D%8B", emojiValue: '🍋' },
+    { discordValue: "%F0%9F%8D%91", emojiValue: '🍑' },
+    { discordValue: "%F0%9F%8D%89", emojiValue: '🍉' },
+    { discordValue: "%F0%9F%8D%8D", emojiValue: '🍍' },
+    { discordValue: "%F0%9F%8D%93", emojiValue: '🍓' }
+]

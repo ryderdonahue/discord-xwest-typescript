cc05e35520a9e61dd026481840fce287af2a0c7f
Ryder Donahue <ryder.donahue@gmail.com>
2021-01-11 21:25:42 -0800

    fixed bad negative formatting

--- src/util.ts
+++ src/util.ts
@@ -2,22 +2,22 @@
 import { Config } from './config';
 
 export function shuffle(a: Array<any>): Array<any> {
-    let j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
 function replaceAll(string, search, replace) {
-    return string.split(search).join(replace);
+  return string.split(search).join(replace);
 }
 
 export function SanitizeMarkdown(str: string): string {
-    return replaceAll(replaceAll(replaceAll(str, '`', ''), '\\', ''), `\n`, '');
+  return replaceAll(replaceAll(replaceAll(str, '`', ''), '\\', ''), `\n`, '');
 }
 
 
@@ -33,8 +33,6 @@ export function formatRate(num: number): string {
   let res = num.toLocaleString();
   if (num > 0) {
     res = "+" + res;
-  }else if (num < 0){
-    res = "-" + res;
   }
 
   return res;
